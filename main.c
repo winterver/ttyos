e35b2b6e97a5cddbb53f3b282f5161eadf7b0c14
winterver <29510769@qq.com>
2025-09-18 10:59:03 +0800

    move entrypgdir to entry.S

--- main.c
+++ main.c
@@ -2,12 +2,6 @@
 #include <types.h>
 #include <printk.h>
 
-ALIGNED(0x1000) u32 pagedir[1024] = {
-    [0] = 0x000000 | 0x80 | 0x2 | 0x1, // identity mapping for 0-4m
-    [1] = 0x400000 | 0x80 | 0x2 | 0x1, // identity mapping for 4-8m
-    [KVADDR>>22] = 0x400000 | 0x80 | 0x2 | 0x1, // map first 4m of kernel to 4-8m
-};
-
 extern int kend[];
 
 void uartinit();
