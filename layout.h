a220e9d33a82509097a304ba2831df799062048a
winterver <29510769@qq.com>
2025-09-30 08:03:35 +0800

    move kernel to 0xc0000000

--- layout.h
+++ layout.h
@@ -1,9 +1,9 @@
 #ifndef __LAYOUT_H__
 #define __LAYOUT_H__
 
-#define VBASE       0x80000000  /* base of virtual address */
+#define VBASE       0xC0000000  /* base of virtual address */
 #define KPADDR      0x100000    /* physical address into which the kernel will be loaded by grub */
-#define KVADDR      0x80100000  /* virtual address of the kernel */
+#define KVADDR      0xC0100000  /* virtual address of the kernel */
 #define KSSIZE      0x4000      /* kernel stack size */
 
 #define IOSPACE     0           /* I/O space is in low mem */
